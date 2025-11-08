bbfba15810930b0a2b74519ec2e4c5dd947c1e31
Viacheslav-Bo <zotikivna@gmail.com>
2025-11-08 12:58:30 +0200

    base2

--- src/main.js
+++ src/main.js
@@ -1,2 +1,16 @@
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
+// Описаний у документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import './js/pixabay-api';
 import './js/render-functions';
+
+// У файлі main.js напиши всю логіку роботи додатка. Виклики нотифікацій iziToast,
+// усі перевірки на довжину масиву в отриманій відповіді робимо саме в цьому файлі.
+// Імпортуй в нього функції із файлів pixabay-api.js та render-functions.js та викликай їх у відповідний момент.
